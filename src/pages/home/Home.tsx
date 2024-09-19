4d8f56a514fc3edbfcc28533934b9b3895123c7d
hs0820 <gustn1029@gmail.com>
2024-09-19 18:00:39 +0900

    메인페이지 컴포넌트로 분리

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import styles from "./home.module.scss";
+import DiaryForm from "./DiaryForm";
+import DiaryItem from "./DiaryItem";
 
 const Home = () => {
   return (
     <div className={styles["container"]}>
-    <main className={styles["diary-main"]}>
-      <h2 className={styles["heart"]}>2023.02.27의 비밀일기</h2>
-
-      <form>
-        <label className="a11y-hidden" htmlFor="diary-title">일기 제목</label>
-        <input className="input-style" id="diary-title" type="text" placeholder="제목" required />
-
-        <label className="a11y-hidden" htmlFor="diary-content">일기 내용</label>
-        <textarea className={styles["diary-textarea"]} id="diary-content" placeholder="오늘의 비밀은 무엇인가요?"></textarea>
-        <button className="black-btn" type="submit">작성하기</button>
-      </form>
-    </main>
+      <main className={styles["diary-main"]}>
+        <h2 className={styles["heart"]}>2023.02.27의 비밀일기</h2>
+        <DiaryForm />
+      </main>
+      <section>
+        <h2 className="a11y-hidden">일기 목록</h2>
+        <ul>{<DiaryItem />}</ul>
+      </section>
     </div>
-  )
+  );
 };
 
 export default Home;
