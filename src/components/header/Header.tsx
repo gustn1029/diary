7e00ef7b5a5540313182d62492a91e139e499599
hs0820 <gustn1029@gmail.com>
2024-09-19 18:01:26 +0900

    상태에 맞게 헤더 출력 변경되도록 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../../hooks/useReduxToolkitHook";
 
 const Header = () => {
+  const { user, isLogin, isAuthLoading } = useAppSelector((state) => state.auth);
   return (
     <header>
-    <div className="header-wrap">
-      <h1><Link to="/"><img className="logo" src="/img/logo.svg" alt="두근두근 비밀일기" /></Link></h1>
-      <div>
-        <Link to="/login" className="btn-login">로그인</Link>
-        <Link to="/signup" className="btn-join">회원가입</Link>
+      <div className="header-wrap">
+        <h1>
+          <Link to="/">
+            <img className="logo" src="/img/logo.svg" alt="두근두근 비밀일기" />
+          </Link>
+        </h1>
+        <div>
+          {isAuthLoading ? (
+            <p>Loading...</p>
+          ) : !isLogin ? (
+            <>
+              <Link to="/login" className="btn-login">
+                로그인
+              </Link>
+              <Link to="/signup" className="btn-join">
+                회원가입
+              </Link>
+            </>
+          ) : (
+            <>
+              <p className="hello">환영합니다. <strong>{user?.displayName}</strong> 님!</p>
+              <Link to="/logout" className="btn-logout">
+                로그아웃
+              </Link>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </header>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
